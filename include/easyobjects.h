e1fd78f727c3a5e94c9f281d87021bced3d811b6
Jason Heflinger <jasonheflinger@gmail.com>
2026-03-04 12:15:49 -0500

    added a def for no get support to allow arrlists of statically sized arrays

--- include/easyobjects.h
+++ include/easyobjects.h
@@ -119,6 +119,95 @@ void ARRLIST_##T##_wipe(ARRLIST_##T* list) { \
 	list->size = 0; \
 }
 
+#define DECLARE_ARRLIST_NO_GET_SUPPORT(T) \
+typedef struct { \
+	size_t size; \
+	size_t maxsize; \
+	T* data; \
+} ARRLIST_##T; \
+\
+void ARRLIST_##T##_zero(ARRLIST_##T* list, size_t size); \
+void ARRLIST_##T##_add(ARRLIST_##T* list, T element); \
+void ARRLIST_##T##_insert(ARRLIST_##T* list, T element, size_t index); \
+int ARRLIST_##T##_has(ARRLIST_##T* list, T element); \
+void ARRLIST_##T##_remove(ARRLIST_##T* list, size_t index); \
+void ARRLIST_##T##_clear(ARRLIST_##T* list); \
+void ARRLIST_##T##_wipe(ARRLIST_##T* list);
+
+#define IMPL_ARRLIST_NO_GET_SUPPORT(T) \
+void ARRLIST_##T##_zero(ARRLIST_##T* list, size_t size) { \
+    ARRLIST_##T##_clear(list); \
+    list->maxsize = size; \
+    list->size = size; \
+    list->data = (T*)EZ_ALLOC(size, sizeof(T)); \
+} \
+\
+void ARRLIST_##T##_add(ARRLIST_##T* list, T element) { \
+	if (list->maxsize == 0) { \
+		list->data = (T*)EZ_ALLOC(1, sizeof(T)); \
+		list->size = 1; \
+		list->maxsize = 1; \
+		memcpy(list->data, &element, sizeof(T)); \
+	} else if (list->size < list->maxsize) { \
+		memcpy(&(list->data[list->size]), &element, sizeof(T)); \
+		list->size++; \
+	} else { \
+		list->maxsize *= 2; \
+        list->data = EZ_REALLOC(list->data, list->maxsize, sizeof(T)); \
+		memcpy(&(list->data[list->size]), &element, sizeof(T)); \
+		list->size++; \
+	} \
+} \
+\
+void ARRLIST_##T##_insert(ARRLIST_##T* list, T element, size_t index) { \
+	if (index > list->size) { \
+		EZ_FATAL("Invalid arraylist index to insert"); \
+	} else if (index == list->size) { \
+		ARRLIST_##T##_add(list, element); \
+	} else { \
+		ARRLIST_##T##_add(list, element); \
+		for (size_t i = list->size; i > 0; i--) { \
+			size_t ind = i - 1; \
+			if (ind == index) { \
+				memcpy(&(list->data[ind]), &element, sizeof(T)); \
+				break; \
+			} else { \
+				memcpy(&(list->data[ind]), &(list->data[ind - 1]), sizeof(T)); \
+			} \
+		} \
+	} \
+} \
+\
+int ARRLIST_##T##_has(ARRLIST_##T* list, T element) { \
+	for (size_t i = 0; i < list->size; i++) \
+		if (memcmp(&element, &(list->data[i]), sizeof(T)) == 0) return TRUE; \
+	return FALSE; \
+} \
+\
+void ARRLIST_##T##_remove(ARRLIST_##T* list, size_t index) { \
+	if (index >= list->size) \
+		EZ_FATAL("Invalid arraylist index to remove"); \
+	if (index == list->size - 1)  { \
+		list->size--; \
+		return; \
+	} \
+	for (size_t i = index; i < list->size - 1; i++) \
+		list->data[i] = list->data[i + 1]; \
+	list->size--; \
+} \
+\
+void ARRLIST_##T##_clear(ARRLIST_##T* list) { \
+	if (list->data != NULL) \
+		EZ_FREE(list->data); \
+	list->data = NULL; \
+	list->size = 0; \
+	list->maxsize = 0; \
+} \
+\
+void ARRLIST_##T##_wipe(ARRLIST_##T* list) { \
+	list->size = 0; \
+}
+
 #define DECLARE_ARRLIST_NAMED(name, T) \
 typedef struct { \
 	size_t size; \
